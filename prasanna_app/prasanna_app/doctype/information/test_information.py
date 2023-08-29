b612ad87c74d456b61dddac384927458cbe5ffc1
sreejani-vankayala <sreejanivankayala@gmail.com>
2023-08-29 09:21:35 +0000

    first commit

---
+++ prasanna_app/prasanna_app/doctype/information/test_information.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, prasanna and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testinformation(FrappeTestCase):
+	pass
